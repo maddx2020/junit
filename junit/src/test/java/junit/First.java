e1f0ec4f6c794715fb0adaca964d7ac7d1a25432
maddx2020 <madd.xx83@gmail.com>
2020-09-04 02:12:21 +0300

    +

--- junit/src/test/java/junit/First.java
+++ junit/src/test/java/junit/First.java
@@ -6,32 +6,26 @@ import org.junit.Test;
 
 // опытный класс - в конце работы переделать как надо
 
-
 public class First extends Assert {
-	
+
 	private Calculator calc;
-	
-	
+
 	@Before
 	public void init() {
-		
+
 		calc = new Calculator();
-		System.out.println("before");
+
 	}
-	
-	
+
 	@Test
 	public void testEqualsPlus() {
 
-		
 		assertEquals(4, calc.getPlus(2, 2));
-		
+
 		assertEquals(20, calc.getPlus(10, 10));
-		
+
 		assertEquals(502, calc.getPlus(500, 2));
-		
-		System.out.println("tests passed");
-		
+
 	}
 
 }
